93c4c4a05c80097b5e75469061ff6159f5e3dec5
Julian Orozco Vanegas <julian.orozco@globant.com>
2024-08-09 10:55:18 -0500

    sum and substract methods

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,2 +1,9 @@
 public class Calculator {
+    public double sum (double a, double b){
+        return a + b;
+    }
+
+    public double subtract (double a, double b){
+        return a - b;
+    }
 }
